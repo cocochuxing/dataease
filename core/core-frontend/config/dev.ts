9d3a82e909c1ede5a03b9c7ffed4942cb2f8c725
zhangchunsheng <zhangchunsheng423@gmail.com>
2026-07-06 18:16:06 +0800

    update

--- core/core-frontend/config/dev.ts
+++ core/core-frontend/config/dev.ts
@@ -2,13 +2,13 @@ export default {
   server: {
     proxy: {
       '/api/f': {
-        target: 'http://localhost:8100',
+        target: 'http://139.199.23.58:8166',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api\/f/, '')
       },
       // 使用 proxy 实例
       '/api': {
-        target: 'http://localhost:8100',
+        target: 'http://139.199.23.58:8166',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, 'de2api')
       }
