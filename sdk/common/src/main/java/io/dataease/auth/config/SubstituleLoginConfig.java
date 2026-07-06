01b584f02d1baf933953241ada6576a111aca558
zhangchunsheng <zhangchunsheng423@gmail.com>
2026-07-06 11:38:55 +0800

    update

--- sdk/common/src/main/java/io/dataease/auth/config/SubstituleLoginConfig.java
+++ sdk/common/src/main/java/io/dataease/auth/config/SubstituleLoginConfig.java
@@ -91,7 +91,7 @@ public class SubstituleLoginConfig {
             if (StringUtils.isNotBlank(configuredPwd)) {
                 pwd = configuredPwd;
             }
-            pwd = "DataEase@123456";
+            pwd = "Data@1110";
             LogUtil.info("substitule login, pwd: {}", pwd);
             String configuredTokenSecret = readString(substituleLoginData, TOKEN_SECRET_KEY);
             if (StringUtils.isNotBlank(configuredTokenSecret)) {
